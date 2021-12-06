612475580b93f9434a504cbd89ac49ca6fcad6ec
d3tu <69614422+d3tu@users.noreply.github.com>
2021-12-06 21:51:47 +0000

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express'),
 app.get('/', (_req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.post('/file.test', (req, res) => {
-  req.on('data', () => void 0);
+  req.once('data', () => void 0);
   req.once('end', () => {
     res.set({ 'content-length': size });
     res.end(buffer);
