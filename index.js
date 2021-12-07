73a8152712b8c4952e36e0e9bc3e776b98e4d25d
Deto <69614422+d3tu@users.noreply.github.com>
2021-12-07 00:19:01 +0000

    Little adjustments.

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ app.post('/file.test', (req, res) => {
   req.once('data', () => void 0);
   req.once('end', async () => {
     let sizeQ = req.query['size'], stop;
-    if (isNaN(sizeQ) || sizeQ < 1 || sizeQ > 100) sizeQ = 1;
-    res.set({ 'content-length': size * (0.1 * sizeQ) });
-    while (--sizeQ) {
+    if (isNaN(sizeQ) || sizeQ < 10) sizeQ = 10;
+    else if (sizeQ > 100) sizeQ = 100;
+    sizeQ = 0.1 * sizeQ;
+    res.set({ 'content-length': size * sizeQ });
+    while (sizeQ--) {
       if (stop) break;
       await new Promise(next => res.write(buffer, err => {
         if (err) stop = true;
         setTimeout(next, 1);
       }));
     }
-    res.end(buffer);
+    res.end();
   });
 });
 
